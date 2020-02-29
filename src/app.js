664a92005d77e0be90bb6cf164a5c08b94870f17
barBossa97 <46846338+barBossa97@users.noreply.github.com>
2020-02-29 18:29:32 +0530

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import './styles/styles.scss';
 
 import {Provider} from 'react-redux';
 import store from "./store/configureStore";
-import {addExpense} from "./actions/expenses";
+// import {addExpense} from "./actions/expenses";
 /*------------------------------------------------------------------------------------------*/
 
 const jsx = (
@@ -16,10 +16,10 @@ const jsx = (
     </Provider>
 );
 
-const exp_1 = store.dispatch(addExpense({title: 'Water bill', note: 'Normal', amount: 600, createdAt: 654321}));
-const exp_2 = store.dispatch(addExpense({title: 'Gas bill', note: 'Low', amount: 400, createdAt: -20000}));
-const exp_3 = store.dispatch(addExpense({title: 'Rent', note: 'Very high', amount: 34000, createdAt: -10000}));
-const exp_4 = store.dispatch(addExpense({title: 'Current bill', amount: 700, createdAt: 123456}));
+// const exp_1 = store.dispatch(addExpense({title: 'Water bill', note: 'Normal', amount: 600, createdAt: 654321}));
+// const exp_2 = store.dispatch(addExpense({title: 'Gas bill', note: 'Low', amount: 400, createdAt: -20000}));
+// const exp_3 = store.dispatch(addExpense({title: 'Rent', note: 'Very high', amount: 34000, createdAt: -10000}));
+// const exp_4 = store.dispatch(addExpense({title: 'Current bill', amount: 700, createdAt: 123456}));
 
 // setTimeout(() => store.dispatch(setTextFilter('bill')), 3000);
 ReactDOM.render(jsx, document.getElementById('app'));
